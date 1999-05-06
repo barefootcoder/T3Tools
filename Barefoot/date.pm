58f8f9bf488cbbe5b11b3ba6914b1ab62376f636
Buddy Burden <barefootocder@gmail.com>
1999-05-06 16:38:22 +0000

    added helper routine _date_parse() and made other routines call it added dayDiff() routine

--- Barefoot/date.pm
+++ Barefoot/date.pm
@@ -29,40 +29,91 @@ use Barefoot::string;
 #
 
 
-# this one just tests for a valid date and returns 1 or 0
-# it accepts the following forms:
+#
+# private "helper" routines
+#
+
+
+# parse a date in one of many formats:
+# (month and day numbers may be only one digit instead of two)
 #	mm/dd/yy
 #	mm/dd/yyyy
 #	mm-dd-yy
 #	mm-dd-yyyy
-# month and day numbers may be only one digit
+#	yyyymmdd			(such as is returned by sortableString()
+# after parsing, it tosses it off to timelocal and returns the time value
+# if you give it an invalid date, it will return undef
+sub _date_parse
+{
+	my ($date) = @_;
+	my ($mon, $day, $year);
+
+	if ($date =~ /^(\d\d\d\d)(\d\d)(\d\d)$/)
+	{
+		$mon = $2;
+		$day = $3;
+		$year = $1;
+	}
+	else
+	{
+		($mon, $day, $year) = split(?/|-?, $date);
+	}
+
+	--$mon;									# timelocal expects this
+	$year -= 1900 if $year >= 100;			# account for 2 or 4 digit years;
+
+	# this will return undef if timegm vomits
+	# we use timegm instead of timelocal to avoid problems with DST
+	return eval { timegm(0,0,0,$day,$mon,$year) };
+}
+
+
+#
+# public routines
+#
+
+
+# this one just tests for a valid date and returns 1 or 0
 sub isValid
 {
 	my ($date) = @_;
 
-	my ($mon, $day, $year) = split(?/|-?, $date);
-	--$mon;									# timelocal expects this
-	$year -= 1900 if $year >= 100;			# account for 2 or 4 digit years;
-	eval { timelocal(0,0,0,$day,$mon,$year) };
-	return $@ ? 0 : 1;
+	return defined(_date_parse($date));
 }
 
 sub incDays
 {
 	my ($date, $inc) = @_;
-	# print "$date, $inc\n";
 
-	my ($year, $mon, $day) = $date =~ /(\d\d\d\d)(\d\d)(\d\d)/;
-	# print "$mon, $day, $year\n";
-
-	$year -= 1900, --$mon;				# adjust values for timelocal
-	my $secs = timelocal(0, 0, 12, $day, $mon, $year);
+	my $secs = _date_parse($date);
 	$secs += ($inc * 24 * 60 * 60);		# increment seconds by that many days
 
-	($day, $mon, $year) = (localtime $secs)[3..5];
+	my ($day, $mon, $year) = (localtime $secs)[3..5];
 	$year += 1900, ++$mon;
 
 	return $year . string::lpad($mon, 2, 0) . string::lpad($day, 2, 0);
+}
+
+# this returns the difference in days between two dates (older date first)
+# it will return undef if either date is invalid
+# it will return a negative number if you put the newer date first
+# if you send it only one date, it will return the difference between that
+# date and today
+sub dayDiff
+{
+	my ($old_date, $new_date) = @_;
+
+	my $old_secs = _date_parse($old_date);
+	# sortableString() is a shortcut for getting today's date
+	# (probably not a very efficient one)
+	my $new_secs = defined($new_date) ? _date_parse($new_date)
+			: _date_parse(sortableString());
+	return undef unless defined($old_secs) and defined($new_secs);
+
+	my $diff_secs = $new_secs - $old_secs;
+	# this should always return an int because the time values were built
+	# (by _date_parse) with no hours, minutes, or seconds
+	return $diff_secs / (24 * 60 * 60);
 }
 
 sub sortableString
@@ -70,7 +121,7 @@ sub sortableString
 	my ($sep) = @_;
 
 	$sep = "" if !defined($sep);		# separator for elements
-	my $date = `date "+%Y$sep%m$sep%d$sep%H$sep%M$sep%S"`;
+	my $date = `date "+%Y$sep%m$sep%d"`;
 	chomp $date;
 	return $date;
 }
