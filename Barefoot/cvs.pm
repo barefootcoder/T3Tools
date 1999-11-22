5594c2aba6499863bd0f7c1f89040e67610f8d0d
Buddy Burden <barefootocder@gmail.com>
1999-11-22 15:02:38 +0000

    Initial revision

---
+++ Barefoot/cvs.pm
@@ -0,0 +1,38 @@
+#! /usr/local/bin/perl
+
+# For RCS:
+# $Date$
+# $Log$
+#
+# $Id$
+# $Revision$
+
+###########################################################################
+#
+# header
+#
+###########################################################################
+#
+# Description of header
+#
+# #########################################################################
+#
+# All the code herein is Class II code according to your software
+# licensing agreement.  Copyright (c) 1999 Barefoot Software.
+#
+###########################################################################
+
+package header;
+
+### Private ###############################################################
+
+use strict;
+
+1;
+
+
+#
+# Subroutines:
+#
+
+
