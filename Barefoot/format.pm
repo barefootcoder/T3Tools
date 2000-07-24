8d5464f6edd09bdd708136a627cb9a6ec17dd86e
Buddy Burden <barefootocder@gmail.com>
2000-07-24 03:33:43 +0000

    Initial revision

---
+++ Barefoot/format.pm
@@ -0,0 +1,37 @@
+#! /usr/local/bin/perl
+
+# For RCS:
+# $Date$
+#
+# $Id$
+# $Revision$
+
+###########################################################################
+#
+# format
+#
+###########################################################################
+#
+# The swrite() sub, which allows you to actually _use_ Perl formats.
+#
+# #########################################################################
+#
+# All the code herein is Class II code according to your software
+# licensing agreement.  Copyright (c) 2000 Barefoot Software.
+#
+###########################################################################
+
+package format;
+
+### Private ###############################################################
+
+use strict;
+
+1;
+
+
+#
+# Subroutines:
+#
+
+
