c0c21f78f740486e7ba23479f462d2d75ebdd1c7
Buddy Burden <barefootocder@gmail.com>
2000-08-28 21:12:40 +0000

    first truly working version implements get_yn() and input()

--- Barefoot/input.pm
+++ Barefoot/input.pm
@@ -3,17 +3,20 @@
 # For RCS:
 # $Date$
 # $Log$
+# Revision 1.1  1999/11/22 16:24:34  buddy
+# Initial revision
+#
 #
 # $Id$
 # $Revision$
 
 ###########################################################################
 #
-# header
+# input
 #
 ###########################################################################
 #
-# Description of header
+# General routines to help with script input.
 #
 # #########################################################################
 #
@@ -22,11 +25,17 @@
 #
 ###########################################################################
 
-package header;
+package Barefoot::input;
 
 ### Private ###############################################################
 
 use strict;
+
+use vars qw(@ISA @EXPORT_OK);
+require Exporter;
+
+@ISA = qw(Exporter);
+@EXPORT_OK = qw(get_yn input);
 
 1;
 
@@ -36,3 +45,29 @@ use strict;
 #
 
 
+sub get_yn
+{
+	my ($prompt) = @_;
+
+	# prompt is optional
+	print $prompt if defined $prompt;
+	print "  [y/N] ";
+
+	my $yn = <STDIN>;
+	return 1 if $yn =~ /^y/i;
+	return 0;
+}
+
+sub input
+{
+	my ($prompt, $default) = @_;
+
+	local ($|) = 1;							# autoflush stdout
+	print $prompt;
+	print " (", $default, ")" if defined($default);
+	print "  " if defined($prompt);
+
+	my $answer = <STDIN>;
+	chomp $answer;
+	return $answer ? $answer : $default;
+}
