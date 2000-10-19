1a655ef998045461d8630e5526bfce6451f96e14
Buddy Burden <barefootocder@gmail.com>
2000-10-19 20:03:34 +0000

    added new format to _date_parse() added new function dateTimeSeconds()

--- Barefoot/date.pm
+++ Barefoot/date.pm
@@ -18,6 +18,7 @@ use strict;
 
 use Time::Local;
 
+use Barefoot::array;
 use Barefoot::string;
 
 
@@ -25,9 +26,11 @@ use enum qw(
 	:PART_		SEC MIN HR DAY MON YR DOW DOY DST
 );
 
-use vars qw(@DAY_NAME);
+use vars qw(@DAY_NAME @MON_ABBREV);
 @DAY_NAME = ('Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday',
 		'Friday', 'Saturday');
+@MON_ABBREV = ('Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug',
+		'Sep', 'Oct', 'Nov', 'Dec');
 
 
 1;
@@ -47,11 +50,17 @@ use vars qw(@DAY_NAME);
 # (month and day numbers may be only one digit instead of two)
 #	mm/dd/yy
 #	mm/dd/yyyy
+#	dd/Mon/yy
+#	dd/Mon/yyyy
 #	mm-dd-yy
 #	mm-dd-yyyy
+#	dd-Mon-yy
+#	dd-Mon-yyyy
 #	yyyymmdd			(such as is returned by sortableString()
-# after parsing, it tosses it off to timelocal and returns the time value
-# if you give it an invalid date, it will return undef
+# after parsing, you can get the month, day, and year, properly adjusted
+# for timelocal() or timegm() (if used in an array context), or the result
+# of timegm() with a time of midnight (if used in a scalar context)
+# if you give it an invalid date, it will return undef or an empty list
 sub _date_parse
 {
 	my ($date) = @_;
@@ -66,14 +75,30 @@ sub _date_parse
 	else
 	{
 		($mon, $day, $year) = split(?/|-?, $date);
+		if ($day =~ /^[A-Z][a-z][a-z]$/)
+		{
+			# this must be dd/Mon/yyyy type format
+			my $mon_abbrev = $day;
+			$day = $mon;
+			# aindex already returns zero-based for month
+			$mon = aindex(@MON_ABBREV, $mon_abbrev);
+		}
+		else
+		{
+			--$mon;							# timegm expects zero-based
+		}
 	}
 
-	--$mon;									# timelocal expects this
 	$year -= 1900 if $year >= 100;			# account for 2 or 4 digit years;
 
 	# this will return undef if timegm vomits
 	# we use timegm instead of timelocal to avoid problems with DST
-	return eval { timegm(0,0,0,$day,$mon,$year) };
+	# if array context is requested, we return month, day, and year
+	# so that the user can send it to timelocal if desired
+	my $time_secs = eval { timegm(0,0,0,$day,$mon,$year) };
+	return defined($time_secs)
+		? (wantarray ? ($mon, $day, $year) : $time_secs)
+		: (wantarray ? () : undef);
 }
 
 
@@ -123,6 +148,52 @@ sub dayDiff
 	# this should always return an int because the time values were built
 	# (by _date_parse) with no hours, minutes, or seconds
 	return $diff_secs / (24 * 60 * 60);
+}
+
+# this routine takes a date and an optional time, and returns the number
+# of seconds since the epoch (same as time() does for the current time)
+# date formats it understands are the same as _date_parse() (which see)
+# time format is one of
+#		hh:mm		(24-hour)
+#		hh:mm:ss	(24-hour)
+#		hh:mmXM		(12-hour)
+#		hh:mm:ssXM	(12-hour)
+#		hh.mm		(24-hour)
+#		hh.mm.ss	(24-hour)
+#		hh.mmXM		(12-hour)
+#		hh.mm.ssXM	(12-hour)
+# where XM is one of AM, PM, am, pm, A, P, a, or p
+sub dateTimeSeconds
+{
+	my ($date, $time) = @_;
+	$time = "0:0" unless $time;					# default time is midnight
+	my $sep = '(?:\.|:)';						# time separator pattern
+	my $ampm = '([AaPp])[mM]?';					# AM/PM marker
+
+	my ($mon, $day, $year) = _date_parse($date);
+	# print "mon $mon, day $day, year $year\n";
+	if ( $year and $time =~ /^(\d+)$sep(\d+)(?:$sep(\d+))?(?:$ampm)?$/ )
+	{
+		my ($hours, $mins, $secs, $meridian) = ($1, $2, $3, $4);
+		$secs = 0 unless $secs;
+		$meridian = lc($meridian);				# for ease of comparison
+		if ($hours == 12 and $meridian eq "a")	# midnight
+		{
+			$hours = 0;
+		}
+		elsif ($hours != 12)					# noon is same either way
+		{
+			$hours += 12 * ($meridian eq "p");	# add 12 if PM
+		}
+
+		# print "hour $hours, min $mins, sec $secs\n";
+		return timelocal($secs, $mins, $hours, $day, $mon, $year);
+	}
+	else
+	{
+		# error
+		return undef;
+	}
 }
 
 sub sortableString
