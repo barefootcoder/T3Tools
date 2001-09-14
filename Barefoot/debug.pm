e7383d511c75c28ac5014bf6ecd72ae337b06600
Buddy Burden <barefootocder@gmail.com>
2001-09-14 17:43:18 +0000

    debug can now be used in -T scripts without tainting @INC

--- Barefoot/debug.pm
+++ Barefoot/debug.pm
@@ -38,12 +38,24 @@ use strict;
 
 use Barefoot::cvs;
 
-use lib (cvs::WORKING_DIR);
+# if you want to test scripts with the -T switch, you're going to have issues.
+# the problem is that cvs::WORKING_DIR is based on an environment variable
+# ($USER), so it is inherently tainted.  thus, to make this work, we're going
+# to need to untaint the personal CVS dir.  as we know the directories _we_
+# added should only contain letters, we can safely assume that a valid
+# cvs::WORKING_DIR would contain only slashes plus whatever a valid Linux
+# username is (which is basically the same as \w).
+BEGIN
+{
+	cvs::WORKING_DIR =~ m@^([/\w]+)$@;
+	# use lib doesn't work here; do it by hand
+	unshift @INC, $1 if $1;
+}
 
 
 $::debug_mode = 1;
 $::debug = $::debug_mode;					# for backwards compatibility
-# note: $debug is depracated.  please use $debug_mode in all new code
+# note: $debug is deprecated.  please use $debug_mode in all new code
 
 1;
 
