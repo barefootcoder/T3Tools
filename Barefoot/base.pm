b3393fafe969375f7f675b2af2ee2c49c58c7913
Buddy Burden <barefootocder@gmail.com>
2001-10-08 02:12:42 +0000

    initial version

---
+++ Barefoot/base.pm
@@ -0,0 +1,40 @@
+#! /usr/local/bin/perl
+
+# For RCS:
+# $Date$
+#
+# $Id$
+# $Revision$
+
+###########################################################################
+#
+# Barefoot::base
+#
+###########################################################################
+#
+# This module should be ideally included by other Barefoot:: modules, but
+# could be included separately if need be.  It defines the DEBUG constant,
+# which is turned on by Barefoot::debug.  It also defines the constants
+# true and false, whose utility should be self-evident.
+#
+# #########################################################################
+#
+# All the code herein is Class II code according to your software
+# licensing agreement.  Copyright (c) 2000 Barefoot Software.
+#
+###########################################################################
+
+package header;
+
+### Private ###############################################################
+
+use strict;
+
+1;
+
+
+#
+# Subroutines:
+#
+
+
