532c0385c6eaa025b011767880562683e8f3afa4
Buddy Burden <barefootocder@gmail.com>
2001-12-02 19:44:37 +0000

    finally worked out the mess with DEBUG

--- Barefoot/base.pm
+++ Barefoot/base.pm
@@ -17,6 +17,8 @@
 # which is turned on by Barefoot::debug.  It also defines the constants
 # true and false, whose utility should be self-evident.
 #
+# See Barefoot::debug for full details on DEBUG.
+#
 # Note that the same chicken-and-egg problem you have with testing
 # Barefoot::debug applies to Barefoot::base as well; you can't get the
 # version of base.pm from your CVS working directory as you can with other
@@ -25,20 +27,25 @@
 # #########################################################################
 #
 # All the code herein is Class II code according to your software
-# licensing agreement.  Copyright (c) 2000 Barefoot Software.
+# licensing agreement.  Copyright (c) 2001 Barefoot Software.
 #
 ###########################################################################
 
-package main;
+package Barefoot::base;
 
 ### Private ###############################################################
 
 use strict;
 
+use base qw(Exporter);
+use vars qw(@EXPORT);
+@EXPORT = qw(true false DEBUG);
 
-use constant DEBUG_MODE => 0;
-use constant true => 1;
-use constant false => 0;
+
+sub true();
+sub false();
+
+sub DEBUG();
 
 
 1;
@@ -48,4 +55,33 @@ use constant false => 0;
 # Subroutines:
 #
 
+sub import
+{
+	my $pkg = shift;
 
+	# print STDERR "here i am in base import!\n";
+	$pkg->export_to_level(1, $pkg, 'true');
+	$pkg->export_to_level(1, $pkg, 'false');
+
+	my $caller_package = caller;
+	# print STDERR "my calling package is $caller_package\n";
+	unless (defined eval "${caller_package}::DEBUG();")
+	{
+		$pkg->export_to_level(1, $pkg, 'DEBUG');
+	}
+}
+
+sub true ()
+{
+	return 1;
+}
+
+sub false ()
+{
+	return 0;
+}
+
+sub DEBUG ()
+{
+	return 0;
+}
