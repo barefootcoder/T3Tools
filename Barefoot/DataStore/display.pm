edc98ce1ab178892365dd97a40480b56f9a7b662
Buddy Burden <barefootocder@gmail.com>
2002-05-24 22:01:45 +0000

    initial version

---
+++ Barefoot/DataStore/display.pm
@@ -0,0 +1,39 @@
+#! /usr/local/bin/perl -w
+
+# For RCS:
+# $Date$
+#
+# $Id$
+# $Revision$
+
+###########################################################################
+#
+# Barefoot::DataStore::display
+#
+###########################################################################
+#
+# The display() function accepts a file which contains mostly basic SQL
+# commands, with the possible addition of some more sophisticated formatting
+# options, and produces a string which is (hopefully) ready for printing.
+#
+# #########################################################################
+#
+# All the code herein is Class II code according to your software
+# licensing agreement.  Copyright (c) 2000 Barefoot Software.
+#
+###########################################################################
+
+package DataStore;
+
+### Private ###############################################################
+
+use strict;
+
+1;
+
+
+#
+# Subroutines:
+#
+
+
