5abd77fc59958f54f941576d99af2e8ffd807231
Buddy Burden <barefootocder@gmail.com>
2002-08-31 20:39:21 +0000

    initial version

---
+++ Barefoot/DataStore/DataRow.pm
@@ -0,0 +1,38 @@
+#! /usr/local/bin/perl -w
+
+# For RCS:
+# $Date$
+#
+# $Id$
+# $Revision$
+
+###########################################################################
+#
+# DataStore::DataRow
+#
+###########################################################################
+#
+# This object allows both name and number access to values, similar to
+# a Perl pseudo-hash (only smarter).
+#
+# #########################################################################
+#
+# All the code herein is Class II code according to your software
+# licensing agreement.  Copyright (c) 2002 Barefoot Software.
+#
+###########################################################################
+
+package DataStore::DataRow;
+
+### Private ###############################################################
+
+use strict;
+
+1;
+
+
+#
+# Subroutines:
+#
+
+
