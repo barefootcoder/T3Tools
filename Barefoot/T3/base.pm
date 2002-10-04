5398914f20cbfecc5ce7529797b42c9f5ee5209d
Buddy Burden <barefootocder@gmail.com>
2002-10-04 20:35:23 +0000

    initial version

---
+++ Barefoot/T3/base.pm
@@ -0,0 +1,42 @@
+#! /usr/local/bin/perl -w
+
+# For RCS:
+# $Date$
+#
+# $Id$
+# $Revision$
+
+###########################################################################
+#
+# Barefoot::T3::base
+#
+###########################################################################
+#
+# This provides basic constants and functions that just about every T3
+# module will use.  Most of these are exported into your namespace whether
+# you like or not, so try to peruse the list carefully before including.
+#
+# #########################################################################
+#
+# All the code herein is Class II code according to your software
+# licensing agreement.  Copyright (c) 2002 Barefoot Software.
+#
+###########################################################################
+
+package header;
+
+### Private ###############################################################
+
+use strict;
+
+
+###########################
+# Subroutines:
+###########################
+
+
+###########################
+# Return a true value:
+###########################
+
+1;
---
+++ Barefoot/T3/valid.pm
@@ -0,0 +1,47 @@
+#! /usr/local/bin/perl -w
+
+# For RCS:
+# $Date$
+#
+# $Id$
+# $Revision$
+
+###########################################################################
+#
+# Barefoot::T3::valid
+#
+###########################################################################
+#
+# This module holds a few validation routines for T3 modules.  These are
+# mainly used by the command-line clients.
+#
+# #########################################################################
+#
+# All the code herein is Class II code according to your software
+# licensing agreement.  Copyright (c) 2002 Barefoot Software.
+#
+###########################################################################
+
+package Barefoot::T3::valid;
+
+### Private ###############################################################
+
+use strict;
+
+use base qw<Exporter>;
+use vars qw<@EXPORT_OK>;
+@EXPORT_OK = qw<>;
+
+use Barefoot::base;
+
+
+###########################
+# Subroutines:
+###########################
+
+
+###########################
+# Return a true value:
+###########################
+
+1;
