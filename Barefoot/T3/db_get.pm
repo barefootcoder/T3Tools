8084d67f2b401584275697b7ff0a1d6544a14827
Buddy Burden <barefootocder@gmail.com>
2002-10-06 17:54:56 +0000

    initial version

---
+++ Barefoot/T3/db_get.pm
@@ -0,0 +1,41 @@
+#! /usr/local/bin/perl -w
+
+# For RCS:
+# $Date$
+#
+# $Id$
+# $Revision$
+
+###########################################################################
+#
+# Barefoot::T3::db_get
+#
+###########################################################################
+#
+# This module contains routines which retreive various information from
+# the T3 database.
+#
+# #########################################################################
+#
+# All the code herein is Class II code according to your software
+# licensing agreement.  Copyright (c) 2002 Barefoot Software.
+#
+###########################################################################
+
+package Barefoot::T3::db_get;
+
+### Private ###############################################################
+
+use strict;
+
+
+###########################
+# Subroutines:
+###########################
+
+
+###########################
+# Return a true value:
+###########################
+
+1;
