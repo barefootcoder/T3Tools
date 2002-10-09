1110de4c48f67714a245ffa729ceaa252072d591
Buddy Burden <barefootocder@gmail.com>
2002-10-09 04:29:13 +0000

    implements &t3 for accessing datastore implements t3_username() to figure out T3 nickname implements t3_filenames to figure out local file and history file

--- Barefoot/T3/base.pm
+++ Barefoot/T3/base.pm
@@ -23,16 +23,91 @@
 #
 ###########################################################################
 
-package header;
+package Barefoot::T3::base;
 
 ### Private ###############################################################
 
 use strict;
 
+use base qw<Exporter>;
+use vars qw<@EXPORT>;
+@EXPORT = qw<t3 t3_username t3_filenames>;
+
+use Barefoot::base;
+
+
+# constants
+
+use constant CONFIG_FILE => '/etc/t3.conf';
+
+use constant DBSERVER_DIRECTIVE => 'DBServer';
+use constant DATABASE_DIRECTIVE => 'Database';
+use constant TIMERDIR_DIRECTIVE => 'TimerDir';
+
+use constant DEFAULT_WORKGROUP => 'Barefoot';
+#use constant DEFAULT_WORKGROUP => 'TestCompany';
+
+
+our $t3;									# DataStore for singleton
+
+our %t3_file_ext =							# extensions for local files
+(
+	timer	=>	'.timer',
+	todo	=>	'.todo',
+);
+
+our %t3_hist_file =							# local history files
+(
+	timer	=>	'timer.history',
+	todo	=>	'todo.history',
+);
+
 
 ###########################
 # Subroutines:
 ###########################
+
+
+sub t3
+{
+	$t3 = DataStore->open(DEBUG ? "t3test" : "T3", $ENV{USER})
+			unless defined $t3;
+	return $t3;
+}
+
+
+sub t3_username
+{
+	die("Invalid user.  Change username or talk to administrator.")
+			unless exists $ENV{T3_USER};
+	return $ENV{T3_USER};
+}
+
+
+sub t3_filenames
+{
+	my ($module, $user) = @_;
+
+	# double check validity of which file
+	# (this indicates a logic error)
+	die("don't know extension for module $module")
+			unless exists $t3_file_ext{$module};
+	die("don't know history file for module $module")
+			unless exists $t3_hist_file{$module};
+
+    my $cfg_file = config_file->read(CONFIG_FILE);
+    my $workgroup = $ENV{T3_WORKGROUP} || DEFAULT_WORKGROUP;
+
+    my $t3dir = $cfg_file->lookup($workgroup, TIMERDIR_DIRECTIVE);
+	die("don't have a directory for timer files") unless $t3dir;
+	die("cannot write to directory $t3dir") unless -d $t3dir and -w $t3dir;
+
+    my $t3file = "$t3dir/$user" . $t3_file_ext{$module};
+    my $histfile = "$t3dir/" . $t3_hist_file{$module};
+	print "timer file is $t3file\n" if DEBUG >= 2;
+
+	return ($t3file, $histfile);
+}
 
 
 ###########################
