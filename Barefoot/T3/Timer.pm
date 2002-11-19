aaf589a54d54286acf14a74b92ef47e01944e780
Buddy Burden <barefootocder@gmail.com>
2002-11-19 20:06:58 +0000

    initial version

---
+++ Barefoot/T3/Timer.pm
@@ -0,0 +1,40 @@
+#! /usr/local/bin/perl -w
+
+# For CVS:
+# $Date$
+#
+# $Id$
+# $Revision$
+
+###########################################################################
+#
+# Barefoot::T3::Timer
+#
+###########################################################################
+#
+# Some general functions that are specific to the Timer module.
+#
+# #########################################################################
+#
+# All the code herein is Class II code according to your software
+# licensing agreement.  Copyright (c) 2002 Barefoot Software.
+#
+###########################################################################
+
+package Barefoot::T3::Timer;
+
+### Private ###############################################################
+
+use strict;
+
+
+###########################
+# Subroutines:
+###########################
+
+
+###########################
+# Return a true value:
+###########################
+
+1;
