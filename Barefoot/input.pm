8656c83ebe373c9f7a21762aed6105ade10e6ff5
Buddy Burden <barefootocder@gmail.com>
2003-06-26 18:03:10 +0000

    added options to menu_select implemented: TMARGIN, LMARGIN, SPBETWEEN, HEADER, and TRUNC_MSG options

--- Barefoot/input.pm
+++ Barefoot/input.pm
@@ -14,15 +14,55 @@
 #
 # General routines to help with script input.
 #
+#
 # get_yn prints a prompt, accepts input, and returns 1 (true) if the input
 # begins with 'y' or 'Y', otherwise returns 0 (false).  Default value is
 # always false.
+#
 #
 # input prints a prompt if given, will return a default value if given and
 # the user provides no input, otherwise returns whatever input the user
 # gives (chomped).
 #
-# menu_select works very much like the "select" command of ksh.
+#
+# menu_select works similarly to the "select" command of ksh, but with more
+# emphasis on _which_ choice you made, as opposed to _what_ the choice is.
+#
+#		my $choice = menu_select("Pick one:", @choices);
+#		print "You chose: $choices[$choice]\n";
+#		# *NOT* print "You chose: $choice\n"; !!
+#
+# You may also use options with menu_select.  To do so, make the final
+# argument (or the last element of your menu items array; menu_select
+# obviously can't tell the difference) a reference to a hash containing the
+# options you want to set.  Like so:
+#
+#		my $choice = menu_select("Choose:", @choices, { LMARGIN => 3 });
+#
+# Here are the valid options menu_select understands:
+#
+#		LMARGIN		=>	Distance from left side of screen for menu (default: 0)
+#		TMARGIN		=>	Number of blank lines before menu (default: 0)
+#		SPBETWEEN	=>	Number of spaces between menu item columns (default: 1)
+#		HEADER		=>	Text to print above the menu.  This must be a single
+#						string, though of course it may be several lines long.
+#						If the header is so long that it cannot fit on the
+#						screen (along with the menu itself and the prompt),
+#						then the header is truncated to a proper number of
+#						lines (see also TRUNC_MSG).  The LMARGIN and TMARGIN
+#						options do *not* apply to the header; if you want
+#						margins for your header, apply them yourself when you
+#						(default: "")
+#						build the string.  HEADER should end with a newline.
+#		TRUNC_MSG	=>	If the header needs to be truncated, this message is
+#						printed at the bottom (i.e., underneath the HEADER,
+#						but before the TMARGIN and menu itself).  For instance,
+#						it might be something like "<More ...>".  The number
+#						of lines in TRUNC_MSG is taken into consideration when
+#						truncating HEADER.  Like HEADER, TRUNC_MSG should end
+#						with a newline.  (default: "")
+#
+#	
 #
 # #########################################################################
 #
@@ -37,17 +77,23 @@ package Barefoot::input;
 
 use strict;
 
+use Term::Size;
 use Data::Dumper;
 use Array::PrintCols;
 
 use Barefoot::base;
 use Barefoot::range;
+use Barefoot::string;
 
 
 use base qw<Exporter>;
 use vars qw<@EXPORT_OK>;
 
 @EXPORT_OK = qw<get_yn input menu_select>;
+
+
+our ($COLS, $ROWS) = Term::Size::chars;
+
 
 1;
 
@@ -119,6 +165,13 @@ sub input
 sub menu_select
 {
 	my ($prompt, @choices) = @_;
+	my $options = {};
+	$options = pop @choices if ref $choices[$#choices] eq "HASH";
+
+	# set defaults in case not specified
+	$options->{LMARGIN} ||= 0;
+	$options->{SPBETWEEN} ||= 1;
+	$options->{TMARGIN} ||= 0;
 
 	my $spec = "%" . length(scalar(@choices)) . "d";
 
@@ -144,11 +197,34 @@ sub menu_select
 
 	# pointless for print_cols to sort our list
 	$Array::PrintCols::PreSorted = true;
+	my $menu = format_cols \@choices, $max_choice_len + $options->{SPBETWEEN},
+			$COLS, $options->{LMARGIN};
+
+	# get header and make sure it will fit on screen
+	my $header = $options->{HEADER} || "";
+	my $hlines = string::count($header, "\n");
+	my $mlines = string::count($menu, "\n");
+	# final 2 is for prompt and empty line above it
+	my $max_hlines = $ROWS - $options->{TMARGIN} - $mlines - 2;
+	if ($hlines > $max_hlines)
+	{
+		if ($options->{TRUNC_MSG})
+		{
+			$max_hlines -= string::count($options->{TRUNC_MSG}, "\n");
+		}
+		else
+		{
+			$options->{TRUNC_MSG} = "";
+		}
+
+		$header =~ /^ ( (.*?\n){$max_hlines} ) /x;
+		$header = $1 . $options->{TRUNC_MSG};
+	}
 
 	MENU: {
-		print_cols \@choices, $max_choice_len + 3, 0, 2;
-
-		print "\n$prompt ";
+		print "$header";
+		print "\n" x $options->{TMARGIN};
+		print "$menu\n$prompt ";
 		$choice = <STDIN>;
 		print "\n";
 
