50b15b0e46eebc3dc5e068c5ee646ce889594b8e
Buddy Burden <barefootocder@gmail.com>
2007-01-22 02:47:21 +0000

    cleaned up code dramatically; rearranged guts to rely more on Date::Format and Date::Parse; updated copyright added new options when use'ing: def_fmt and def_tfmt to control default date and datetime formats (respectively) depracated dateTimeSeconds() removed sortableString(), as no one appears to have been using it for years started on system to allow later modules to change options, but only if not overriden by the user

--- Barefoot/date.pm
+++ Barefoot/date.pm
@@ -1,5 +1,3 @@
-#! /usr/local/bin/perl
-
 ###########################################################################
 #
 # date
@@ -12,7 +10,7 @@
 #
 # All the code herein is released under the Artistic License
 #		( http://www.perl.com/language/misc/Artistic.html )
-# Copyright (c) 1999-2003 Barefoot Software, Copyright (c) 2004-2006 ThinkGeek
+# Copyright (c) 1999-2007 Barefoot Software, Copyright (c) 2004-2006 ThinkGeek
 #
 ###########################################################################
 
@@ -25,19 +23,18 @@ use warnings;
 
 use Carp;
 use Time::Local;
+use Date::Parse;
+use Date::Format;
 
-use Barefoot::base;
+use Barefoot;
 use Barefoot::array;
 use Barefoot::string;
 
 
-use enum qw(
-	:PART_		SEC MIN HR DAY MON YR DOW DOY DST
-);
+use enum qw< :PART_		SEC MIN HR DAY MON YR DOW DOY DST >;
 
 
-our @DayName = ('Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday',
-		'Friday', 'Saturday');
+our @DayName = ('Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday');
 
 our %DayNumbers =
 (
@@ -50,8 +47,7 @@ our %DayNumbers =
 	SAT		=>	6,
 );
 
-our @MonAbbrev = ('Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug',
-		'Sep', 'Oct', 'Nov', 'Dec');
+our @MonAbbrev = ('Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec');
 
 our %MonNumbers =
 (
@@ -71,7 +67,11 @@ our %MonNumbers =
 
 our %Options =
 (
+	def_fmt		=>	'%Y-%m-%d',
+	def_tfmt	=>	'%Y-%m-%d %T',
 	epoch		=>	'1/1/1980',
+
+	overriden	=>	{},
 );
 
 
@@ -80,135 +80,23 @@ our %Options =
 ###########################
 
 
-#
-# import function
-#
-
-package Barefoot::date;
-use Carp;
-use Barefoot::base;
-
-sub import
-{
-	my $class = shift;
-	my (%opts) = @_;
-
-	print STDERR "in import for date.pm\n" if DEBUG >= 5;
-	foreach (keys %opts)
-	{
-		print STDERR "trying to set option $_\n" if DEBUG >= 4;
-		if (exists $Options{$_})
-		{
-			$Options{$_} = $opts{$_};
-		}
-		else
-		{
-			croak("cannot set unknown option $_ in date module");
-		}
-	}
-}
-
-# back to "regular" package
-package date;
-
-
 ###########################
 # private "helper" routines
 ###########################
 
 
-# parse a date in one of many formats:
-# (month and day numbers may be only one digit instead of two)
-#	mm/dd/yy
-#	mm/dd/yyyy
-#	dd/Mon/yy
-#	dd/Mon/yyyy
-#	yyyy/mm/dd
-#	mm-dd-yy
-#	mm-dd-yyyy
-#	dd-Mon-yy
-#	dd-Mon-yyyy
-#	yyyy-mm-dd
-#	yyyymmdd			(such as is returned by sortableString())
-#	Mon dd yyyy
-#	Mon dd yyyy 12:00AM	(such as is returned by Sybase)
-# after parsing, you can get the month, day, and year, properly adjusted
-# for timelocal() or timegm() (if used in an array context), or the result
-# of timegm() with a time of midnight (if used in a scalar context)
-# if you give it an invalid date, it will return undef or an empty list
-
-sub _date_parse
+sub _cvt_date_if_necessary
 {
-	my ($date, $raw_time_okay) = @_;
-	croak("call to date routine with no date given") unless $date;
-	my ($mon, $day, $year);
-
-	# don't really care if this fails
-	$date =~ s/ 12:00AM$//;
+	my ($date) = @_;
 
 	if ($date =~ /^\d{9,10}$/)
 	{
-		# raw epoch seconds value; just pass it through if those are okay
-		return $raw_time_okay ? $date : undef;
-	}
-	elsif ($date =~ /^(\d\d\d\d)(\d\d)(\d\d)$/)
-	{
-		$mon = $2 - 1;
-		$day = $3;
-		$year = $1;
-	}
-	elsif ($date =~ /^(...)  ?(\d?\d) (\d\d\d\d)$/)
-	{
-		return undef unless exists $MonNumbers{uc($1)};
-		$mon = $MonNumbers{uc($1)};
-		$day = $2;
-		$year = $3;
-	}
-	else
-	{
-		($mon, $day, $year) = split(m</|->, $date);
-		# if we don't get all three parts, might as well bail now
-		return undef unless defined $mon and defined $day and defined $year;
-
-		if ($day =~ /^[A-Z][a-z][a-z]$/)
-		{
-			# this must be dd/Mon/yyyy type format
-			my $mon_abbrev = $day;
-			$day = $mon;
-			# this already returns zero-based for month
-			$mon = $MonNumbers{uc($mon_abbrev)};
-		}
-		elsif ($mon > 1000)
-		{
-			# must be yyyy/mm/dd type format
-			($year, $mon, $day) = ($mon, $day, $year);
-			--$mon;							# timegm expects zero-based
-		}
-		else
-		{
-			--$mon;							# timegm expects zero-based
-		}
+		# already epoch seconds; no need to convert
+		return $date;
 	}
 
-	if ($year >= 100)
-	{
-		$year -= 1900;						# account for 2 or 4 digit years
-	}
-	elsif ($year <= 50)
-	{
-		$year += 100;						# nice if can say 01 for 2001
-	}
-
-	print STDERR "_date_parse: mon $mon, day $day, year $year\n" if DEBUG >= 2;
-
-	# this will return undef if timegm vomits
-	# we use timegm instead of timelocal to avoid problems with DST
-	# if array context is requested, we return month, day, and year
-	# so that the user can send it to timelocal if desired
-	my $time_secs = eval { timegm(0,0,0,$day,$mon,$year) };
-	return defined($time_secs)
-		? (wantarray ? ($mon, $day, $year) : $time_secs)
-		: (wantarray ? () : undef);
+	# else just use str2time
+	return str2time($date);
 }
 
 
@@ -222,16 +110,13 @@ sub isValid
 {
 	my ($date) = @_;
 
-	return defined(_date_parse($date));
+	return defined(str2time($date));
 }
 
 
 sub mdy
 {
-	my ($day, $mon, $year) = (gmtime $_[0])[3..5];
-	$year += 1900, ++$mon;
-	#return "$mon/$day/$year";
-	return sprintf("%04d-%02d-%02d", $year, $mon, $day);
+	return time2str($Options{'def_fmt'}, $_[0]);
 }
 
 
@@ -244,11 +129,12 @@ sub today
 sub incDays
 {
 	my ($date, $inc) = @_;
+	debuggit(3 => "incDays: args are", $date, $inc);
 
-	my $secs = _date_parse($date, true);
-	print STDERR "seconds before increment: $secs\n" if DEBUG >= 3;
-	$secs += ($inc * 24 * 60 * 60);		# increment seconds by that many days
-	print STDERR "seconds after increment:  $secs\n" if DEBUG >= 3;
+	my $secs = _cvt_date_if_necessary($date);
+	debuggit(3 => "seconds before increment:", $secs);
+	$secs += ($inc * 24 * 60 * 60);										# increment seconds by that many days
+	debuggit(3 => "seconds after increment:", $secs);
 
 	return mdy($secs);
 }
@@ -257,23 +143,17 @@ sub incDays
 # this returns the difference in days between two dates (older date first)
 # it will return undef if either date is invalid
 # it will return a negative number if you put the newer date first
-# if you send it only one date, it will return the difference between that
-# date and today
+# if you send it only one date, it will return the difference between that date and today
 sub dayDiff
 {
 	my ($old_date, $new_date) = @_;
 
-	my $old_secs = _date_parse($old_date, true);
-	# sortableString() is a shortcut for getting today's date
-	# (probably not a very efficient one)
-	my $new_secs = defined($new_date) ? _date_parse($new_date, true)
-			: _date_parse(sortableString());
+	my $old_secs = _cvt_date_if_necessary($old_date);
+	my $new_secs = defined($new_date) ? _cvt_date_if_necessary($new_date) : time();
 	return undef unless defined($old_secs) and defined($new_secs);
 
 	my $diff_secs = $new_secs - $old_secs;
-	# this should always return an int because the time values were built
-	# (by _date_parse) with no hours, minutes, or seconds
-	return $diff_secs / (24 * 60 * 60);
+	return int($diff_secs / (24 * 60 * 60));
 }
 
 
@@ -293,45 +173,10 @@ sub dayDiff
 sub dateTimeSeconds
 {
 	my ($date, $time) = @_;
-	$time = "0:0" unless $time;					# default time is midnight
-	my $sep = '(?:\.|:)';						# time separator pattern
-	my $ampm = '([AaPp])[mM]?';					# AM/PM marker
 
-	my ($mon, $day, $year) = _date_parse($date);
-	# print "mon $mon, day $day, year $year\n";
-	if ( $year and $time =~ /^(\d+)$sep(\d+)(?:$sep(\d+))?(?:$ampm)?$/ )
-	{
-		my ($hours, $mins, $secs, $meridian) = ($1, $2, $3, $4);
-		$secs = 0 unless $secs;
-		$meridian = lc($meridian);				# for ease of comparison
-		if ($hours == 12 and $meridian eq "a")	# midnight
-		{
-			$hours = 0;
-		}
-		elsif ($hours != 12)					# noon is same either way
-		{
-			$hours += 12 * ($meridian eq "p");	# add 12 if PM
-		}
-
-		# print "hour $hours, min $mins, sec $secs\n";
-		return timelocal($secs, $mins, $hours, $day, $mon, $year);
-	}
-	else
-	{
-		# error
-		return undef;
-	}
-}
-
-
-sub sortableString
-{
-	my ($sep) = @_;
-
-	$sep = "" if !defined($sep);		# separator for elements
-	my $date = `date "+%Y$sep%m$sep%d"`;
-	chomp $date;
-	return $date;
+	print STDERR "Barefoot::date: dateTimeSeconds() is depracated; use Date::Parse's str2time() instead\n";
+	$date = "$date $time" if $time;
+	return str2time($date);
 }
 
 
@@ -360,17 +205,15 @@ sub MondayDate
 #
 # the period functions
 #
-# #########################################################################
-#
+###########################################################################
 
 sub period_num
 {
 	my ($date, $period_len, $epoch) = @_;
-	$epoch = $Options{epoch} unless defined $epoch;
-	print STDERR "period_num: using epoch $epoch\n" if DEBUG >= 3;
+	$epoch = $Options{'epoch'} unless defined $epoch;
+	debuggit(3 => "period_num: using epoch", $epoch);
 
-	print STDERR "period_num: ",
-			int(dayDiff($epoch, $date) / $period_len), "\n" if DEBUG >= 3;
+	debuggit(3 => "period_num: ", int(dayDiff($epoch, $date) / $period_len));
 	return int(dayDiff($epoch, $date) / $period_len);
 }
 
@@ -378,14 +221,50 @@ sub period_num
 sub period_name
 {
 	my ($period_num, $period_len, $epoch) = @_;
-	$epoch = $Options{epoch} unless defined $epoch;
-	print STDERR "period_num: using epoch $epoch\n" if DEBUG >= 3;
+	$epoch = $Options{'epoch'} unless defined $epoch;
+	debuggit(3 => "period_num: using epoch", $epoch);
 
 	my $start = incDays($epoch, $period_num * $period_len);
 	my $end = incDays($start, $period_len - 1);
-	print STDERR "period_name: start is $start, end is $end\n" if DEBUG >= 3;
+	debuggit(3 => "period_name: start is", $start, "/ end is", $end);
 
 	return "$start - $end";
+}
+
+
+###########################################################################
+#
+# import function (must be in "real" package as opposed to date::
+#
+###########################################################################
+
+package Barefoot::date;
+
+use strict;
+use warnings;
+
+use Carp;
+
+use Barefoot;
+
+sub import
+{
+	my ($class, %opts) = @_;
+
+	debuggit(5 => "in import for date.pm");
+	foreach (keys %opts)
+	{
+		debuggit(4 => "trying to set option", $_);
+		if (exists $Options{$_})
+		{
+			$Options{$_} = $opts{$_};
+			$Options{'overriden'}->{$_} = 1;
+		}
+		else
+		{
+			croak("cannot set unknown option $_ in date module");
+		}
+	}
 }
 
 
